f42b451378f872366bd2a023aa7b98bbcbb65376
Tinh <lizidiot1190@gmail.com>
2021-07-24 20:16:57 +0700

    css

---
+++ CarShowroom/DAO/ProductDAO.cs
@@ -0,0 +1,124 @@
+﻿using CarShowroom.Models;
+using Model.EF;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+
+namespace CarShowroom.DAO
+{
+    public class ProductDAO
+    {
+        private ApplicationDbContext db;
+        public ProductDAO()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        public long Insert(Product entity)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(entity.imageFile.FileName);
+            string extension = Path.GetExtension(entity.imageFile.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            entity.image = "~/assets/img/product/" + fileName;
+            fileName = Path.Combine(HttpContext.Current.Server.MapPath("~/assets/img/product/"), fileName);
+            entity.imageFile.SaveAs(fileName);
+            db.Products.Add(entity);
+            db.SaveChanges();
+            return entity.id;
+        }
+
+        public bool Update(Product entity)
+        {
+            if (entity.imageFile == null)
+            {
+                try
+                {
+                    var product = db.Products.Find(entity.id);
+                    product.metaTitle = entity.metaTitle;
+                    product.name = entity.name;
+                    product.loaiXe = entity.loaiXe;
+                    product.loaiThung = entity.loaiThung;
+                    product.taiTrong = entity.taiTrong;
+                    product.namSX = entity.namSX;
+                    product.nhapKhau = entity.nhapKhau;
+                    product.nhienLieu = entity.nhienLieu;
+                    product.description = entity.description;
+                    product.price = entity.price;
+                    product.promotionPrice = entity.promotionPrice;
+                    product.categoryID = entity.categoryID;
+                    product.status = entity.status;
+                    product.topHot = entity.topHot;
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                try
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(entity.imageFile.FileName);
+                    string extension = Path.GetExtension(entity.imageFile.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    entity.image = "~/assets/img/product/" + fileName;
+                    fileName = Path.Combine(HttpContext.Current.Server.MapPath("~/assets/img/product/"), fileName);
+                    var product = db.Products.Find(entity.id);
+                    product.metaTitle = entity.metaTitle;
+                    product.name = entity.name;
+                    product.loaiXe = entity.loaiXe;
+                    product.loaiThung = entity.loaiThung;
+                    product.taiTrong = entity.taiTrong;
+                    product.namSX = entity.namSX;
+                    product.nhapKhau = entity.nhapKhau;
+                    product.nhienLieu = entity.nhienLieu;
+                    product.description = entity.description;
+                    product.image = entity.image;
+                    product.imageFile = entity.imageFile;
+                    product.imageFile.SaveAs(fileName);
+                    product.price = entity.price;
+                    product.promotionPrice = entity.promotionPrice;
+                    product.categoryID = entity.categoryID;
+                    product.status = entity.status;
+                    product.topHot = entity.topHot;
+                    db.SaveChanges();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var product = db.Products.Find(id);
+                db.Products.Remove(product);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable<Product> ListAllPage(int page, int pageSize)
+        {
+            return db.Products.OrderByDescending(p => p.categoryID).ToPagedList(page, pageSize);
+        }
+
+        public Product ViewDetail(int id)
+        {
+            return db.Products.Find(id);
+        }
+    }
+}
--- CarShowroom/Models/Product.cs
+++ CarShowroom/Models/Product.cs
@@ -5,6 +5,7 @@ namespace Model.EF
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
+    using System.Web;
 
     [Table("Product")]
     public partial class Product
@@ -17,15 +18,28 @@ namespace Model.EF
         [StringLength(250)]
         public string name { get; set; }
 
-        [StringLength(500)]
-        public string productInfo { get; set; }
+        [StringLength(255)]
+        public string loaiXe { get; set; }
+        [StringLength(255)]
+        public string loaiThung { get; set; }
+        [StringLength(255)]
+        public string taiTrong { get; set; }
+        [StringLength(255)]
+        public string namSX { get; set; }
+        [StringLength(255)]
+        public string nhapKhau { get; set; }
+        [StringLength(255)]
+        public string nhienLieu { get; set; }
+        [StringLength(255)]
+        public string hopSo { get; set; }
 
         [StringLength(500)]
         public string description { get; set; }
 
         [StringLength(250)]
         public string image { get; set; }
-
+        [NotMapped]
+        public HttpPostedFileBase imageFile { get; set; }
         public decimal? price { get; set; }
 
         public decimal? promotionPrice { get; set; }
